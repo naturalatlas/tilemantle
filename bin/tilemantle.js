22e29e703a97ad3e3195b17d405ece338f1de723
abc3660170 <3413218@qq.com>
2017-12-09 22:39:22 +0800

    1.add one test geojson for test 2.merge FeatureCollection

--- bin/tilemantle.js
+++ bin/tilemantle.js
@@ -51,7 +51,7 @@ function displayHelp() {
 	console.log('');
 }
 
-if (argv.help) {
+if (argv.h) {
 	displayHelp();
 	process.exit(0);
 }
@@ -99,37 +99,43 @@ async.series([
 	},
 	function determineGeometry(callback) {
 		if (rawgeojson) {
-			geojson = JSON.parse(rawgeojson);
-		} else if (argv.file) {
+            geojson = JSON.parse(rawgeojson);
+		} else if (argv.f) {
 			geojson = JSON.parse(fs.readFileSync(argv.file, 'utf8'));
-		} else if (argv.point) {
+		} else if (argv.p) {
 			var coords = String(argv.point).split(',').map(parseFloat);
 			geojson = turf.point([coords[1], coords[0]]);
-		} else if (argv.extent) {
+		} else if (argv.e) {
 			var coords = String(argv.extent).split(',').map(parseFloat);
-			var geojson = turf.featureCollection([
+			geojson = turf.featureCollection([
 				turf.point([coords[1], coords[0]]),
 				turf.point([coords[3], coords[2]])
 			]);
-			//geojson = turf.extent(input);
+            geojson = turf.bboxPolygon(turf.bbox(geojson));
 		} else {
 			displayHelp();
 			console.error('No geometry provided. Pipe geojson, or use --point or --extent');
 			return process.exit(1);
 		}
 
-		if (argv.buffer) {
+		if (argv.b) {
 			var radius = parseFloat(argv.buffer);
 			var units = /mi$/.test(argv.buffer) ? 'miles' : 'kilometers';
 			geojson = turf.buffer(geojson, radius, units);
 		}
 
-		// tilecover doesn't like features
-		geojson = turf.union(geojson.features);
+        // tilecover doesn't like features
+		if(geojson.type === 'FeatureCollection'){
+			var merged = turf.clone(geojson.features[0]),features = geojson.features;
+			for(var i = 0; i < features.length; i++){
+				var poly = features[i];
+                if (poly.geometry) merged = turf.union(merged, poly);
+			}
+            geojson = merged;
+		}
 		if (geojson.type === 'Feature') {
 			geojson = geojson.geometry;
 		}
-
 		callback();
 	},
 	function performAction(callback) {
--- test/turf.js
+++ test/turf.js
@@ -1,16 +1,19 @@
 var turf = require('@turf/turf');
-var featrue = turf.point([118,34],{"name":"测试"},{id:"123",bbox:[1,2,3,4]});
-var featrue2 = turf.point([118,32],{"name":"测试"},{id:"2",bbox:[1,2,3,4]});
-var featrue3 = turf.point([115,32],{"name":"测试"},{id:"43",bbox:[1,2,3,4]});
-var featrue4 = turf.point([116,33],{"name":"测试"},{id:"12",bbox:[1,2,3,4]});
+var tileCover = require("@mapbox/tile-cover");
+var featrue = turf.point([-179,99.01],{"name":"test"},{id:"123",bbox:[1,2,3,4]});
+var featrue2 = turf.point([118,32],{"name":"test"},{id:"2",bbox:[1,2,3,4]});
+var featrue3 = turf.point([115,32],{"name":"test"},{id:"43",bbox:[1,2,3,4]});
+var featrue4 = turf.point([179.99,-85.01],{"name":"test"},{id:"12",bbox:[1,2,3,4]});
 console.log("point test:",featrue);
 var featureCollection = turf.featureCollection([featrue,featrue2,featrue3,featrue4]);
-console.log("featureCollection test:",featureCollection);
+//console.log("featureCollection test:",featureCollection);
 
 var bbox = turf.bbox(featureCollection);
-console.log("bbox test:",bbox);
+//console.log("bbox test:",bbox);
 var bboxPolygon = turf.bboxPolygon(bbox);
-console.log("bboxPolygon test:",bboxPolygon.geometry.coordinates);
+console.log("bboxPolygon test:",bboxPolygon);
+var tileArray = tileCover.tiles(bboxPolygon.geometry,{min_zoom:1,max_zoom:1})
+//console.log("tileCover test:",tileArray)
 
 
 
